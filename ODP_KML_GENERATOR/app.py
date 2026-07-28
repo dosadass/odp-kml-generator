5dc4252242319372fcb945af88db0024eba9e7cb
dosadass <ilyaswangsitsadewo@gmail.com>
2026-07-28 17:57:13 +0700

    Update app.py

--- ODP_KML_GENERATOR/app.py
+++ ODP_KML_GENERATOR/app.py
@@ -493,49 +493,49 @@ if uploaded_file:
                             file_name="ODP_Master.kmz"
                         )
 
-        with right:
-
-            st.subheader("☁️ 3. Informasi Publish")
-
-            if publish:
-            token = st.secrets["GITHUB_TOKEN"]
-            repo = st.secrets["GITHUB_REPO"]
-            branch = st.secrets["GITHUB_BRANCH"]
-            
-
-            with open(kmz_path, "rb") as file:
-                content = base64.b64encode(file.read()).decode()
-            
-            url = f"https://api.github.com/repos/{repo}/contents/ODP_Master.kmz"
-            
-            headers = {
-                "Authorization": f"Bearer {token}",
-                "Accept": "application/vnd.github+json"
-            }
-
-
-            
-            get = requests.get(url, headers=headers)
-            
-            sha = None
-            
-            if get.status_code == 200:
-                sha = get.json()["sha"]
-            
-            payload = {
-                "message": f"Update KMZ {today}",
-                "content": content,
-                "branch": branch
-            }
-            
-            if sha:
-                payload["sha"] = sha
-            
-            response = requests.put(
-                url,
-                headers=headers,
-                json=payload
-            )
+            with right:
+    
+                st.subheader("☁️ 3. Informasi Publish")
+    
+                if publish:
+                token = st.secrets["GITHUB_TOKEN"]
+                repo = st.secrets["GITHUB_REPO"]
+                branch = st.secrets["GITHUB_BRANCH"]
+                
+    
+                with open(kmz_path, "rb") as file:
+                    content = base64.b64encode(file.read()).decode()
+                
+                url = f"https://api.github.com/repos/{repo}/contents/ODP_Master.kmz"
+                
+                headers = {
+                    "Authorization": f"Bearer {token}",
+                    "Accept": "application/vnd.github+json"
+                }
+    
+    
+                
+                get = requests.get(url, headers=headers)
+                
+                sha = None
+                
+                if get.status_code == 200:
+                    sha = get.json()["sha"]
+                
+                payload = {
+                    "message": f"Update KMZ {today}",
+                    "content": content,
+                    "branch": branch
+                }
+                
+                if sha:
+                    payload["sha"] = sha
+                
+                response = requests.put(
+                    url,
+                    headers=headers,
+                    json=payload
+                )
             
             if response.status_code in [200,201]:
                 st.success("✔️ Publish berhasil!")
